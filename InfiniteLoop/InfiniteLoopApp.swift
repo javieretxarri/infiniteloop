a26229b2cced4fcff0df98f2d026e62e35501e11
Javier Etxarri <javieretxarrimarin@gmail.com>
2023-03-28 11:29:38 +0200

    Initial Commit

---
+++ InfiniteLoop/InfiniteLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteLoopApp.swift
+//  InfiniteLoop
+//
+//  Created by Javier Etxarri on 28/3/23.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
